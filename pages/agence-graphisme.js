f3127962ddcdba6757b87799328fcbe696a3f463
Mohammed Abded Hassani Idrissi <72313863+hiabed@users.noreply.github.com>
2025-05-29 19:58:24 +0000

    agence graphism on going

--- pages/agence-graphisme.js
+++ pages/agence-graphisme.js
@@ -9,10 +9,15 @@ function AgenceGraphisme() { // Renamed for clarity if this is intended to be a 
     <div>
       <section className={styles.header}>
         <div className={styles.container}>
-          <h1 className={styles.title}>replace me</h1>
-          <h2 className={styles.subtitle}>replace me</h2>
-          <p className={styles.paragraph}>replace me</p>
-          <button className={styles.contact_button}>Nous contacter</button>
+          <h1 className={styles.title}>Studio graphique | Agence de graphisme</h1>
+          <h2 className={styles.subtitle}>Un logo percutant pour rassurer ses clients</h2>
+          <p className={styles.paragraph}>Unique et impactant, un logo pro vous permet de vous différencier et de marquer les esprits de vos clients.</p>
+          <div className={styles.buttons}>
+            <button className={styles.contact_button}>Recevoir nos offres</button>
+            <button className={styles.contact_button}>Prendre rendez-vous</button>
+          </div>
+          <img className={styles.logos} src='https://www.aleo.agency/_nuxt/img/be232eb.png'></img>
+          <img className={styles.woman} src='https://www.aleo.agency/_nuxt/img/d16665b.png'></img>
         </div>
       </section>
       {/* <FAQList /> */}
